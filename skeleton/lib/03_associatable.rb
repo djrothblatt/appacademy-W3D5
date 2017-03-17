66b2d42956011e94ec0bca01c2ff62284caef1dd
Daniel Rothblatt <djrothblatt@gmail.com>
2017-03-17 16:06:53 -0400

    Implement has_many association and modify belongs_to

--- skeleton/lib/03_associatable.rb
+++ skeleton/lib/03_associatable.rb
@@ -36,9 +36,9 @@ class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
     @name = name
     defaults = {
-      foreign_key: "#{self_class_name.downcase}_id".to_sym,
-      primary_key: "id".to_sym,
-      class_name: name.singularize.camelcase
+      foreign_key: "#{self_class_name.to_s.downcase}_id".to_sym,
+      primary_key: :id,
+      class_name: name.to_s.singularize.camelcase
     }
     merged_options = defaults.merge(options)
     @foreign_key = merged_options[:foreign_key]
@@ -49,8 +49,11 @@ end
 
 module Associatable
   # Phase IIIb
+  # because this will *extend* SQLObject, these methods are *class* methods
+  # so when self is passed into HasManyOptions.new, it refers to the *class*,
   def belongs_to(name, options = {})
     options = BelongsToOptions.new(name, options)
+    assoc_options[name] = options
     define_method(name) do
       foreign_key = self.send(options.foreign_key)
       target_class = options.model_class
@@ -59,15 +62,23 @@ module Associatable
   end
 
   def has_many(name, options = {})
-    # ...
+    options = HasManyOptions.new(name, self, options)
+    define_method(name) do
+      primary_key = self.id
+      source_class = options.model_class
+      source_class.where(options.foreign_key => primary_key)
+      # foreign_key = source_class.send(options.foreign_key)
+      # foreign_key = self.send(options.foreign_key)
+      # target_class = options.model_class
+      # target_class.where(options.primary_key.to_sym => foreign_key)
+    end
   end
 
   def assoc_options
-    # Wait to implement this in Phase IVa. Modify `belongs_to`, too.
+    @assoc_options ||= {}
   end
 end
 
 class SQLObject
   extend Associatable
-  # Mixin Associatable here...
 end
