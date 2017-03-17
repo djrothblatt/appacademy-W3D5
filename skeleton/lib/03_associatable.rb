1c7ea6719e2ec79fb2355d2689a9cba2011507b1
Daniel Rothblatt <djrothblatt@gmail.com>
2017-03-17 14:53:04 -0400

    Get options for associations

--- skeleton/lib/03_associatable.rb
+++ skeleton/lib/03_associatable.rb
@@ -10,23 +10,40 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    class_name.constantize
   end
 
   def table_name
-    # ...
+    model_class.table_name || class_name.tableize
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    defaults = {
+      foreign_key: "#{name}_id".to_sym,
+      primary_key: "id".to_sym,
+      class_name: name.camelcase
+    }
+    merged_options = defaults.merge(options)
+    @foreign_key = merged_options[:foreign_key]
+    @class_name = merged_options[:class_name]
+    @primary_key = merged_options[:primary_key]
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    @name = name
+    defaults = {
+      foreign_key: "#{self_class_name.downcase}_id".to_sym,
+      primary_key: "id".to_sym,
+      class_name: name.singularize.camelcase
+    }
+    merged_options = defaults.merge(options)
+    @foreign_key = merged_options[:foreign_key]
+    @class_name = merged_options[:class_name]
+    @primary_key = merged_options[:primary_key]
   end
 end
 
