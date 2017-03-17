b4e82a0447d2b8c64452a21f115ac00223d29044
Daniel Rothblatt <djrothblatt@gmail.com>
2017-03-17 14:04:18 -0400

    Search SQL objects with WHERE

--- skeleton/lib/02_searchable.rb
+++ skeleton/lib/02_searchable.rb
@@ -3,10 +3,20 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    keys, vals = params.keys, params.values
+    where_string = keys.map { |key| "#{key} = ?" }.join(" AND ")
+    data = DBConnection.execute(<<-SQL, *vals)
+      SELECT
+        *
+      FROM
+        #{table_name}
+      WHERE
+        #{where_string}
+    SQL
+    parse_all(data)
   end
 end
 
 class SQLObject
-  # Mixin Searchable here...
+  extend Searchable
 end
